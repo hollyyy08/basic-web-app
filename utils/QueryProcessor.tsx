1b618831307f2ff04f147cc78c6dd0b0c6520ead
hollyyy08 <hollyhanasakii@gmail.com>
2023-09-19 15:55:06 -0400

    added the plus query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,5 +19,19 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if(query.toLowerCase().includes("Which of the following numbers is the largest: 2, 8, 6?")){
+    return(
+      "8"
+    );
+  }
+
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
   return "";
+
+  
 }
